b84c26d0cb961352cea58b712cbb621f36f1c803
Leirda01 <adrieldj@orange.fr>
2020-06-02 15:26:34 +0200

    Remove unnecessary manual code to handle signals

--- scripts/Pong.gd
+++ scripts/Pong.gd
@@ -4,25 +4,15 @@ const target: = 3
 var score: = 0
 var locked: = true
 
-func _ready():
-	if $ScoreHandlers/ScoreLuc.connect("body_entered", self, "adriel_touched"):
-		printerr("unable to connect 'body_entered' from 'ScoreLuc'")
-
-	if $ScoreHandlers/ScoreAdriel.connect("body_entered", self, "luc_touched"):
-		printerr("unable to connect 'body_entered' from 'ScoreAdriel'")
-
-	if $ScoreHandlers/ScoreLock.connect("body_entered", self, "reset_lock"):
-		printerr("unable to connect 'body_entered' from 'ScoreLock'")
-
 
 func _process(delta):
 	if Input.is_key_pressed(KEY_R):
 		get_tree().reload_current_scene()
 	if Input.is_action_just_pressed("increase") and score < target - 1:
-		reset_lock(null)
+		reset()
 		score(1)
 	if Input.is_action_just_pressed("decrease") and -score < target - 1:
-		reset_lock(null)
+		reset()
 		score(-1)
 
 	if $Ball.sleeping and Input.is_action_just_released("ui_accept"):
@@ -32,15 +22,7 @@ func _process(delta):
 			start_game()
 
 
-func adriel_touched(_body: Ball):
-	score(-1)
-
-
-func luc_touched(_body: Ball):
-	score(1)
-
-
-func reset_lock(_body: Ball):
+func reset():
 	locked = false
 	if abs(score) >= target:
 		spawn_crown()
@@ -57,7 +39,6 @@ func score(point):
 
 func start_game():
 	$Ball.launch()
-	set_display()
 
 
 func set_display():
@@ -65,9 +46,21 @@ func set_display():
 
 
 func spawn_crown():
-	var my_crown = preload("res://scenes/effects/Crown.tscn").instance()
-	add_child(my_crown)
+	var MyCrown = preload("res://scenes/effects/Crown.tscn").instance()
+	add_child(MyCrown)
 	if score > 0:
 		$Crown.set_color($Paddles/PaddleAdriel.color)
 	else:
 		$Crown.set_color($Paddles/PaddleLuc.color)
+
+
+func _on_ScoreLock_body_entered(body):
+	reset()
+
+
+func _on_ScoreLuc_body_entered(body):
+	score(-1)
+
+
+func _on_ScoreAdriel_body_entered(body):
+	score(1)
