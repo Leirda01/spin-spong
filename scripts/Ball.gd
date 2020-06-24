1d405e6745aa2f68e10f9dd3b3f7d9dcf192b4e6
Leirda01 <adrieldj@orange.fr>
2020-06-24 17:01:44 +0200

    Stops ball before crown but out of paddle range

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -14,9 +14,9 @@ func _ready():
 
 func _integrate_forces(_state):
 	if linear_velocity.normalized().dot(Vector2.UP) > 0.8:
-		linear_velocity = Vector2.UP * min_speed
+		linear_velocity = Vector2.UP
 	if linear_velocity.normalized().dot(Vector2.DOWN) > 0.8:
-		linear_velocity = Vector2.DOWN * min_speed
+		linear_velocity = Vector2.DOWN
 
 	linear_velocity = linear_velocity.clamped(max_speed)
 	if linear_velocity.length() < min_speed:
