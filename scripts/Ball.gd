e0f5b10e004895f2557b073553650f1607ef8cb3
DianaJuniper <lucdeligne@hotmail.com>
2020-10-03 17:29:50 +0100

    Tests get_contact_pos() + visual fixes and tweaks

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -14,13 +14,16 @@ func _ready():
 	self.stop()
 
 
-func _integrate_forces(_state):
+func _integrate_forces(state):
 	if linear_velocity.normalized().dot(Vector2.UP) > 0.8:
 		linear_velocity = Vector2.UP
 	if linear_velocity.normalized().dot(Vector2.DOWN) > 0.8:
 		linear_velocity = Vector2.DOWN
-
 	linear_velocity = linear_velocity.normalized() * speed
+	if state.get_contact_count() > 0 :
+		print(" ")
+		print(state.get_contact_local_position(0))
+		print(state.get_contact_collider_object(0).name)
 
 
 func launch():
