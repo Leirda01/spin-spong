fe93dd4620e652a71dd396e7319f85214ee33d85
Leirda01 <adrieldj@orange.fr>
2020-11-21 00:16:41 +0100

    Stick to Luc's way of resizing for resolution

--- scripts/Resolution.gd
+++ scripts/Resolution.gd
@@ -1,20 +1,7 @@
 # original code by CowThing: https://github.com/godotengine/godot/issues/6506
 # port to Godot 3 and bugfixes by atomius.
-
-# usage:
-# set this script as AutoLoad (Singleton enabled)
-#
-# in the project settings:
-# [display]
-# set 'window/size/width' and 'window/size/height' to your desired render resolution.
-# set 'window/size/test_width' and 'window/size/test_height' to the initial window size.
-# set 'window/stretch/mode' to "viewport"
-#
-# [rendering]
-# set 'quality/2d/use_pixel_snap' to true.
-
-
 extends Node
+
 
 onready var root = get_tree().root
 onready var base_size = root.get_visible_rect().size
@@ -28,23 +15,20 @@ func _ready():
 
 
 func _on_screen_resized():
-	var new_window_size = OS.window_size
-	
-	var scale_w = max(int(new_window_size.x / base_size.x), 1)
-	var scale_h = max(int(new_window_size.y / base_size.y), 1)
-	var scale = min(scale_w, scale_h)
-	
-	var diff = new_window_size - (base_size * scale)
-	var diffhalf = (diff * 0.5).floor()
-	
-	root.set_attach_to_screen_rect(Rect2(diffhalf, base_size * scale))
-	
+	var screen_size = OS.window_size
+
+	var ratio = screen_size / base_size
+	var factor = max(1, min(floor(ratio.x), floor(ratio.y)))
+	var new_size = base_size * factor
+	var offset = (screen_size - new_size) / 2
+
+	root.set_attach_to_screen_rect(Rect2(offset, new_size))
+
+	# prevent negative values as they make the bars go in the wrong direction.
+	var bars_size = Vector2(max(offset.x, 0), max(offset.y, 0))
+
 	# Black bars to prevent flickering:
-	var odd_offset = Vector2(int(new_window_size.x) % 2, int(new_window_size.y) % 2)
-	
-	VisualServer.black_bars_set_margins(
-		max(diffhalf.x, 0), # prevent negative values, they make the black bars go in the wrong direction.
-		max(diffhalf.y, 0),
-		max(diffhalf.x, 0) + odd_offset.x,
-		max(diffhalf.y, 0) + odd_offset.y
+	VisualServer.black_bars_set_margins(bars_size.x, bars_size.y,
+		bars_size.x + int(screen_size.x) % 2, # Don't miss a pixel!
+		bars_size.y + int(screen_size.y) % 2
 	)
