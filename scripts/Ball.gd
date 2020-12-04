3da7491a823ac05e6a60fa5d8c720389b9e161b3
Leirda01 <adrieldj@orange.fr>
2020-12-04 17:32:42 +0100

    Add constant instead of magic value
    Simplify bouncing mechanism while preserving old behavior.

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -6,23 +6,22 @@ export var base_speed: int
 export var max_speed: int
 export(Color) var color
 
+const MINDIR := 0.3 # Minimal direction between 0.0 and 1.0
 var speed
 
 
 func _ready():
 	speed = base_speed
 	material.set_shader_param("targ_color", color)
-
 	linear_velocity = Vector2.LEFT
 	self.stop()
 	self.custom_integrator = true
 
 
 func _integrate_forces(state):
-	if linear_velocity.normalized().dot(Vector2.UP) > 0.8:
-		linear_velocity = Vector2.UP
-	if linear_velocity.normalized().dot(Vector2.DOWN) > 0.8:
-		linear_velocity = Vector2.DOWN
+	var direction := linear_velocity.normalized()
+	if abs(direction.dot(Vector2.RIGHT)) <= MINDIR:
+		linear_velocity = (Vector2.UP if direction.y < 0 else Vector2.DOWN)
 	linear_velocity = linear_velocity.normalized() * speed
 
 	if state.get_contact_count() > 0 :
@@ -34,6 +33,7 @@ func _integrate_forces(state):
 		if !effect.empty() :
 			effect["position"] = state.get_contact_local_position(0)
 			owner.add_child(Effect.create_effect(effect))
+
 
 func launch():
 	linear_velocity += Vector2.ZERO
